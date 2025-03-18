d1ec462f30389aac977302e21e08898adb33d1e4
tiziasan <tiziano.santilli@student.univaq.it>
2025-03-18 11:23:15 +0100

    Update sample_3.java

--- Task 2/Code/sample_3.java
+++ Task 2/Code/sample_3.java
@@ -21,7 +21,6 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	// display list of employees
 	@GetMapping("/")
 	public String viewHomePage(Model model) {
 		return findPaginated(1, "firstName", "asc", model);		
@@ -29,7 +28,6 @@ public class EmployeeController {
 	
 	@GetMapping("/showNewEmployeeForm")
 	public String showNewEmployeeForm(Model model) {
-		// create model attribute to bind form data
 		Employee employee = new Employee();
 		model.addAttribute("employee", employee);
 		return "new_employee";
@@ -37,7 +35,6 @@ public class EmployeeController {
 	
 	@PostMapping("/saveEmployee")
 	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
-		// save employee to database
 		employeeService.saveEmployee(employee);
 		return "redirect:/";
 	}
@@ -45,10 +42,8 @@ public class EmployeeController {
 	@GetMapping("/showFormForUpdate/{id}")
 	public String showFormForUpdate(@PathVariable ( value = "id") long id, Model model) {
 		
-		// get employee from the service
 		Employee employee = employeeService.getEmployeeById(id);
 		
-		// set employee as a model attribute to pre-populate the form
 		model.addAttribute("employee", employee);
 		return "update_employee";
 	}
@@ -56,7 +51,6 @@ public class EmployeeController {
 	@GetMapping("/deleteEmployee/{id}")
 	public String deleteEmployee(@PathVariable (value = "id") long id) {
 		
-		// call delete employee method 
 		this.employeeService.deleteEmployeeById(id);
 		return "redirect:/";
 	}
